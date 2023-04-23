08a5971f530c37da64b2e084d28fb16a497d276b
ExoskyCode <exoskyv2@gmail.com>
2023-04-23 10:00:35 +0200

    💄 [FRONT] FriendRequests responsive

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -74,14 +74,14 @@ const FriendRequests = ({sessionId, incomingFriendRequests}: FriendRequestsProps
                <p className={'text-sm'}>Aucune demande d'amis</p>
            ): (
                friendRequests.map((request) => (
-                   <div key={request.senderId} className={'flex gap-4 items-center'}
+                   <div key={request.senderId} className={'flex gap-2 items-center'}
                         >
                        <UserPlus className={'text-white'}/>
-                       <p className={'font-medium text-lg'}>{request.senderEmail}</p>
-                       <button onClick={() => acceptFriend(request.senderId)} aria-label={"accept friend"} className={'w-8 h-8 bg-[#10A37F] hover:bg-[#16bd93] grid place-items-center rounded-full transition hover:shadow-md'}>
+                       <p className={'font-medium text-sm lg:text-lg'}>{request.senderEmail}</p>
+                       <button onClick={() => acceptFriend(request.senderId)} aria-label={"accept friend"} className={'md:w-8 md:h-8 w-6 h-6 bg-[#10A37F] hover:bg-[#16bd93] grid place-items-center rounded-full transition hover:shadow-md'}>
                            <Check className={'font-semibold text-white w-3/4 h-3/4'} />
                        </button>
-                       <button aria-label={"deny friend"} className={'w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md'}
+                       <button aria-label={"deny friend"} className={'md:w-8 md:h-8 w-6 h-6 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md'}
                                onClick={() => denyFriend(request.senderId)}>
                            <X className={'font-semibold text-white w-3/4 h-3/4'} />
                        </button>
