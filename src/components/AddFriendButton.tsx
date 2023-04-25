f726fbd704eaa84c4398838080fc63777ac9a766
Exosky12 <102408092+Exosky12@users.noreply.github.com>
2023-04-25 08:36:53 +0200

    Update AddFriendButton.tsx

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -68,7 +68,7 @@ const AddFriendButton = () => {
                     name='email'
                     type='text'
                     className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#10A37F] sm:text-sm sm:leading-6'
-                    placeholder='toi@example.com'
+                    placeholder='toi@exemple.com'
                 />
                 <Button>Ajouter</Button>
             </div>
